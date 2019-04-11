06383559b3561a2b1872dc4e1632e5be0ef96516
oneilld136 <oneilld136@gmail.com>
2019-04-11 18:03:23 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma (phrase)
   if phrase == "I LOVE YOU GRANDMA!"
      return "I LOVE YOU TOO PUMPKIN!"
+   end
    else phrase == phrase.downcase
      return "HUH?! SPEAK UP, SONNY!"
    elsif phrase == phrase.upcase
