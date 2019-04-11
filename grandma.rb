988a198b38b6ce3a4a33e2863685bb9b2436184b
oneilld136 <oneilld136@gmail.com>
2019-04-11 16:45:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,7 @@ def speak_to_grandma (phrase)
 if phrase == "I LOVE YOU GRANDMA"
   return "I LOVE YOU TOO PUMPKIN!"
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 elsif phrase == phrase.downcase
   return "HUH?! SPEAK UP SONNY!"
@@ -19,9 +19,9 @@ else
   return "NO, NOT SINCE 1938!"
 end
 
-  
-  
-  
+
+
+
 
 # NO, NOT SINCE 1938!
 
