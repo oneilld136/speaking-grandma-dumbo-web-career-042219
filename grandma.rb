86fca11ec3b610b25b02811d0d7b4870598b4fee
oneilld136 <oneilld136@gmail.com>
2019-04-11 16:54:38 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -9,12 +9,7 @@ if phrase == "I LOVE YOU GRANDMA"
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
-elsif phrase == phrase.upcase
-   return "NO, NOT SINCE 1938!"
- else
-   return "HUH?! SPEAK UP, SONNY!"
- end
-end
+
 
 # NO, NOT SINCE 1938!
 
