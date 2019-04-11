e174eb0ed7bb4c20773f875df282c7bc770a39de
oneilld136 <oneilld136@gmail.com>
2019-04-11 16:55:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,12 +4,12 @@ def speak_to_grandma (phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-if phrase == "I LOVE YOU GRANDMA"
-  return "I LOVE YOU TOO PUMPKIN!"
+#if phrase == "I LOVE YOU GRANDMA"
+  #return "I LOVE YOU TOO PUMPKIN!"
 
 # If you shout, she can hear you (or at least she thinks so)
 # and yells back
-end
+
 
 
 # NO, NOT SINCE 1938!
